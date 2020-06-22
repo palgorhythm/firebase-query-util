b4a8a5ac9ec222e1fc587ddd0d0f052371f7dac5
palgorhythm <jacob@mothership.com>
2020-06-22 09:37:54 -0700

    set up config

--- databaseInit.js
+++ databaseInit.js
@@ -1,6 +1,7 @@
-const admin = require('firebase-admin');
-const firebase = require('firebase');
-require('dotenv').config();
+const admin = require('firebase-admin')
+const firebase = require('firebase')
+const path = require('path')
+require('dotenv').config({ path: path.resolve(process.cwd(), '.env.prod') })
 
 const firebaseAppConfig = {
   credential: admin.credential.cert({
@@ -9,14 +10,14 @@ const firebaseAppConfig = {
     projectId: process.env.FIREBASE_PROJECT_ID,
   }),
   databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com/`,
-};
-admin.initializeApp(firebaseAppConfig);
+}
+admin.initializeApp(firebaseAppConfig)
 firebaseApp = firebase.initializeApp(
   {
     apiKey: process.env.FIREBASE_API_KEY,
     databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com/`,
   },
   '[PRODUCTION-DEFAULT]'
-);
+)
 
-module.exports = (ref) => admin.database().ref(ref);
+module.exports = (ref) => admin.database().ref(ref)
--- index.js
+++ index.js
@@ -1,11 +1,48 @@
+const util = require('util');
 const Database = require('./databaseInit');
-const tableName = '';
+const { errorMonitor } = require('stream');
 
-const fetch = async () => {
-  const query = Database(tableName).limitToFirst(10).once('value');
-  const results = (await query).val();
-  console.log(results);
-  return;
+const log = (myObject) => {
+  console.log(util.inspect(myObject, false, null, true /* enable colors */));
 };
 
-fetch();
+const dictionaryToArray = (dictionary) => {
+  const keys = Object.keys(dictionary);
+  const results = [];
+  for (const key of keys) {
+    const currentResult = { key, data: dictionary[key] };
+    results.push(currentResult);
+  }
+  return results;
+};
+
+const fetchBatchFromTable = async (tableName, batchSize) => {
+  const query = Database(tableName)
+    .orderByKey()
+    .limitToFirst(batchSize)
+    .once('value');
+  const resultDict = (await query).val();
+  return dictionaryToArray(resultDict);
+};
+
+const query = async (tableName, batchSize, filterFunc, stringToLog) => {
+  if(batchSize > 5000){
+    throw Error('max batch size 5000!!')
+  }
+  const results = await fetchBatchFromTable(tableName, batchSize);
+  const filteredResults = filterFunc(results);
+
+  console.log(
+    `~~~~~~~~~~~~~~~~~${filteredResults.length} RESULTS RETURNED FOR QUERY: ${stringToLog}~~~~~~~~~~~~~~~~~~~~`
+  );
+  return filteredResults;
+};
+
+const genericFilter = (condition) => {
+  return (results) =>
+    results.filter((result) => {
+      return condition(result);
+    });
+};
+
+module.exports = { query, genericFilter, log, dictionaryToArray };
